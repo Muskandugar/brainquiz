b4fc98dace6ae2584639033c2e3dc7bfa24dd5d8
MUSKAN RANJEET DUGAR <95470565+Muskandugar@users.noreply.github.com>
2022-11-29 00:45:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,114 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'Which is the Biggest continent in the world?',
+    answers: [
+      { text: 'Asia', correct: true },
+      { text: 'North America', correct: false },
+      {text:'Africa',correct:false},
+      {text:'Australia',correct:false}
+    ]
+  },
+  {
+    question: 'Which is the 29th state of India?',
+    answers: [
+      { text: 'Uttarakhand', correct: false },
+      { text: 'Telangana', correct: true },
+      { text: 'Madhya Pradesh', correct: false },
+      { text: 'Uttar Pradesh', correct:false }
+    ]
+  },
+  {
+    question: 'Who was India’s first Deputy Prime Minister?',
+    answers: [
+      { text: 'Morarji Desa', correct: false },
+      { text: 'Vallabhbhai Patel  ', correct: true },
+      { text: 'Devi Lal', correct: false },
+      { text: 'Charan Singh', correct: false }
+    ]
+  },
+  {
+    question: 'What is 4 * 2?',
+    answers: [
+      { text: '6', correct: false },
+      { text: '8', correct: true }
+    ]
+  }
+]
